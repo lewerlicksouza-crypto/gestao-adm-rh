1efd5530025fae59351b745d704610423c436be8
lewerlicksouza-crypto <lewerlicksouza@gmail.com>
2026-04-03 20:49:53 -0300

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,5 +1,17 @@
 import { publicProcedure, router } from "./trpc.js";
 
+const employeesMock = [
+  {
+    id: 1,
+    fullName: "Sebastião Expedito de Freitas Neto",
+    email: "sebastiaofreitas@contasolucoes.com.br",
+    phone: "(22) 3822-2919",
+    jobTitle: "Programador",
+    department: "Suprimentos",
+    isActive: true,
+  },
+];
+
 export const appRouter = router({
   system: router({
     ping: publicProcedure.query(() => {
@@ -7,6 +19,12 @@ export const appRouter = router({
         ok: true,
         message: "API tRPC online",
       };
+    }),
+  }),
+
+  employees: router({
+    list: publicProcedure.query(() => {
+      return employeesMock;
     }),
   }),
 });
