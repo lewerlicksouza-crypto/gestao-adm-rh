dae36575243b322202b2d6a3f0cc8ff7bd3b32b6
lewerlicksouza-crypto <lewerlicksouza@gmail.com>
2026-04-03 22:44:56 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,30 @@
-import { Router, Route } from "wouter";
-import Home from "./pages/Home";
+import { Router, Route, useLocation } from "wouter";
+import { useEffect } from "react";
 import AdminDashboard from "./pages/AdminDashboard";
 import PublicCard from "./pages/PublicCard";
+import Home from "./pages/Home";
+
+function RedirectToAdmin() {
+  const [, navigate] = useLocation();
+
+  useEffect(() => {
+    navigate("/admin");
+  }, [navigate]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <Router>
-      <Route path="/" component={Home} />
+      <Route path="/" component={RedirectToAdmin} />
+      <Route path="/home" component={Home} />
       <Route path="/card/:id" component={PublicCard} />
       <Route path="/admin" component={AdminDashboard} />
-      <Route path="/:rest*" component={() => <div className="p-8">Página não encontrada</div>} />
+      <Route
+        path="/:rest*"
+        component={() => <div className="p-8">Página não encontrada</div>}
+      />
     </Router>
   );
 }
