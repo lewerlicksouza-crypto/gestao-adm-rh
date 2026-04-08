d36c7588430f95c96ab9e9d9040e7553e890dc35
lewerlicksouza-crypto <lewerlicksouza@gmail.com>
2026-04-07 22:19:47 -0300

    Update types.ts

--- shared/types.ts
+++ shared/types.ts
@@ -1,19 +1,9 @@
-export interface Employee {
+export type Employee = {
   id: number;
   fullName: string;
   email: string;
-  phone?: string;
-  position: string;
-  company?: string;
-  status: "active" | "inactive";
-}
-
-export interface Vacation {
-  id: number;
-  employeeId: number;
-  startDate: string;
-  endDate: string;
-  type: "30" | "20+10" | "15+15";
-  status: "pending" | "approved" | "rejected";
-  notes?: string;
-}
+  phone: string;
+  jobTitle: string;
+  department: string;
+  status: string;
+};
